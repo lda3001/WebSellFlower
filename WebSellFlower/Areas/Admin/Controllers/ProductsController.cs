189c3a91aa2a0e4039c971ca3017354e8c662f55
ducanhdz <leanhthuongx7@gmail.com>
2024-11-27 18:28:48 +0700

    Updated Index.cshtml, _Layout.cshtml, and Details.cshtml files with new script sections and modified existing code. Changed connection string timeout in appsettings.json. Added and modified files in wwwroot/assets/js directory.

---
+++ WebSellFlower/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebSellFlower.Models;
+
+namespace WebSellFlower.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ProductsController : Controller
+	{
+		private readonly WebsiteBanHoaContext _context;
+		public ProductsController(WebsiteBanHoaContext context)
+		{
+			_context = context;
+		}
+		[Route("admin/product/{alias}-{id}.html")]
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.TblProducts == null)
+			{
+				return NotFound();
+			}
+
+			var product = await _context.TblProducts.Include(i => i.CategoryProd)
+				.FirstOrDefaultAsync(m => m.ProdId == id);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			ViewBag.productReview = _context.TblProductReviews.
+				Where(i => i.ProdId == id && i.IsActive).ToList();
+
+			ViewBag.productRelated = _context.TblProducts.
+				Where(i => i.ProdId != id && i.CategoryProdId == product.CategoryProdId).Take(4)
+				.OrderByDescending(i => i.ProdId).ToList();
+
+			return View(product);
+		}
+
+		[Route("admin/product-list.html")]
+		public async Task<IActionResult> List()
+		{
+			if (_context.TblProducts == null)
+			{
+				return NotFound();
+			}
+
+			var product = await _context.TblProducts
+			.Include(p => p.CategoryProd)
+			.Select(p => new ProductDto 
+			{ 
+				ProdId = p.ProdId, 
+				ProdName = p.ProdName, 
+				CategoryName = p.CategoryProd.CategoryProdName 
+			})
+			.ToListAsync();
+
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			ViewBag.products = product;
+
+
+			return View();
+		}
+		[Route("admin/product/edit/{id}")]
+		public async Task<IActionResult> Edit(int id)
+		{
+			var product = await _context.TblProducts.Include(i => i.CategoryProd)
+				.FirstOrDefaultAsync(m => m.ProdId == id);
+
+			if (product == null)
+				return NotFound();
+
+			return View(product);
+		}
+
+	}
+	public class ProductDto 
+	{
+		public int ProdId { get; set; }
+    	public string ProdName { get; set; }
+		public string CategoryName { get; set; }
+	}
+}
--- WebSellFlower/wwwroot/assets/js/forms/quill-init.js
+++ WebSellFlower/wwwroot/assets/js/forms/quill-init.js
@@ -1,3 +1,11 @@
 var quill = new Quill("#editor", {
   theme: "snow",
 });
+
+// const content = [
+//   { insert: 'Hello ' },
+//   { insert: 'world!', attributes: { bold: true } },
+//   { insert: '\n' }
+// ];
+
+// quill.setContents(content);
