ea4b3dde74818daea1bf7a27e3686ae01a781644
tuananhysl <anhx2004@gmail.com>
2024-12-17 19:57:19 +0700

    contact

---
+++ WebSellFlower/Models/TblContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebSellFlower.Models;
+
+public partial class TblContact
+{
+    public int ContactId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Message { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+}
--- WebSellFlower/obj/Debug/net8.0/WebSellFlower.AssemblyInfo.cs
+++ WebSellFlower/obj/Debug/net8.0/WebSellFlower.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebSellFlower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6afaa6e9ab6b9fd8b4f711e5392a8bb0f96d228")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab18fbdd99c3bd1189463338cfac351853ab50d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebSellFlower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebSellFlower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
